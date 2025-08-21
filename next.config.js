d5a34f45d49236aa9396992af421d9a58e40f44c
theIndrajeet <121814014+theIndrajeet@users.noreply.github.com>
2025-08-21 14:35:53 +0530

    Configure Next.js static export for Netlify

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   // Allow loading data from our GitHub repo
   async headers() {
     return [
